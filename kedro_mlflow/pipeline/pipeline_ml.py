b76d396aeaaa626534040e4240d43cf7dfa442de
manuel.giambi <manuel.giambi@ki-insurance.com>
2025-12-12 17:01:18 +0800

    PC-3999 Remove check consistency (temporary!!)

--- kedro_mlflow/pipeline/pipeline_ml.py
+++ kedro_mlflow/pipeline/pipeline_ml.py
@@ -91,9 +91,8 @@ class PipelineML(Pipeline):
         self.log_model_kwargs = {**self.LOG_MODEL_KWARGS_DEFAULT, **log_model_kwargs}
         self.hooks = hooks
 
-        # skip consistency check if VertexAIPipelinesRunner is used -- fixes mismatch when running using --nodes
-        if type(kpm_kwargs['runner']).__qualname__ != "VertexAIPipelinesRunner":
-            self._check_consistency()
+        # TODO temporary!! restore
+        # self._check_consistency()
 
     @property
     def _logger(self) -> Logger:
